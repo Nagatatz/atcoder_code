d8a27db20765813c5a8b9fea3387aac511b5965d
Tsubasa Nagata <nagata.tsubasa@unipro.co.jp>
2020-05-20 00:06:56 +0900

    ABC 142 C [Boot camp for Beginners Easy 100] #16

--- app/abc/142/C.hs
+++ app/abc/142/C.hs
@@ -1,4 +1,5 @@
 import qualified Data.ByteString.Char8 as BS
+import Data.Function
 import Data.List
 import Data.Maybe
 
@@ -8,18 +9,7 @@ readIntList = map readInt . BS.words
 getInt = readInt <$> BS.getLine
 getIntList = readIntList <$> BS.getLine
 
-sortList :: [Int] -> Int -> [Int]
-sortList xs 0 = []
-sortList xs n = sortList xs (n -1) ++ [fromJust (elemIndex n xs) + 1]
-
-convertListAsString :: [Int] -> String
-convertListAsString [] = ""
-convertListAsString [x] = show x
-convertListAsString (x : xs) = show x ++ " " ++ convertListAsString xs
-
 main = do
   n <- getInt
-  as <- getIntList
-  sortList as n
-  let numList = sortList as n
-  putStrLn $ convertListAsString numList
+  xs <- getIntList
+  putStrLn $ unwords (map (show . fst) (sortBy (compare `on` snd) (zip xs [1 .. n])))
