5fa7674cdeae7925332d3acca1610df2820ec1ea
Tsubasa Nagata <nagata.hbdc@gmail.com>
2020-06-02 00:23:11 +0900

    ABC 169 D

--- app/abc/169/D.hs
+++ app/abc/169/D.hs
@@ -1,60 +1,47 @@
--- TLE
 import qualified Data.ByteString.Char8 as BS
-import Data.Maybe
 import Data.List
+import Data.Maybe
 
-readInt = fst . fromJust . BS.readInteger
+readInt = fst . fromJust . BS.readInt
 
 getInt = readInt <$> BS.getLine
 
-factors :: Integer -> [Integer]
-factors n = [x | x <- [1..n], n `mod` x == 0]
-
-factorization :: Integer -> [Integer]
-factorization 1 = []
-factorization x = v : factorization (x `div` v)
+-- https://qiita.com/little_Haskeller/items/614a3ae20a517c19bb1f
+primes :: Integral a => [a]
+primes = map fromIntegral $ [2, 3] ++ primes'
   where
-    v = (factors x) !! 1
+    primes' = [5] ++ f 1 7 primes'
+    f m s (p : ps) = [n | n <- ns, gcd m n == 1] ++ f (m * p) (p * p) ps
+      where
+        ns = [x + y | x <- [s, s + 6 .. p * p - 2], y <- [0, 4]]
 
-createTuple :: [Integer] -> [(Integer, Int)]
-createTuple [] = []
-createTuple xs = e : createTuple xs2
+divn :: Int -> Int -> Int
+divn x a
+  | divided > 0 && x `mod` a == 0 = 1 + divn divided a
+  | otherwise = 0
   where
-    num = head xs
-    (xs1, xs2) = span (==num) xs
-    e = (num, length xs1)
+    divided = x `div` a
 
-partition_of_integer :: Int -> [[Int]]
-partition_of_integer n = part_int n n [] []
+fac :: Int -> [Int] -> Int -> [(Int, Int)]
+fac 1 _ _ = []
+fac x (p : ps) n
+  | p > n = [(x, 1)]
+  | otherwise = tup ++ fac x' ps n
   where
-    part_int 0 _ xs ys = (reverse xs):ys
-    part_int 1 _ xs ys = (reverse (1:xs)):ys
-    part_int n 1 xs ys = (reverse (replicate n 1 ++ xs)):ys
-    part_int n k xs ys =
-      let ys' = part_int n (k - 1) xs ys
-      in if n - k >= 0 then part_int (n - k) k (k:xs) ys' else ys'
+    count = divn x p
+    (tup, x') =
+      if count == 0
+        then ([], x)
+        else ([(p, count)], x `div` p ^ count)
 
-allDifferent :: (Eq a) => [a] -> Bool
-allDifferent []     = True
-allDifferent (x:xs) = x `notElem` xs && allDifferent xs
-
-checkParts :: [[Int]] -> Int
-checkParts [] = 1
-checkParts (x:xs) | allDifferent x = length x
-                  | otherwise = checkParts xs
-
-calc :: [(Integer, Int)] -> Int
+calc :: [Int] -> Int
 calc [] = 0
-calc (t:ts) = k + calc ts
-  where
-    c = snd t
-    k = if c == 1
-      then 1
-      else checkParts(reverse (partition_of_integer c))
+calc (t : ts) = length (takeWhile (<= t) [i * (i + 1) `div` 2 | i <- [1 ..]]) + calc ts
 
 main = do
   n <- getInt
   if n == 1
-  then print 0
-  else print $ calc (createTuple (factorization n))
-   
+    then print 0
+    else do
+      let s = floor (sqrt (fromIntegral n))
+      print $ calc (map snd (fac n primes s))
