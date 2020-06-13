467e04c9bf81eca799bb3974b52a253d79c54cfa
Tsubasa Nagata <nagata.hbdc@gmail.com>
2020-06-13 09:14:07 +0900

    ABC 075 B [Boot camp for Beginners Medium 100] #6 共通化

--- app/abc/075/B.hs
+++ app/abc/075/B.hs
@@ -13,14 +13,8 @@ getNString n = map readString <$> replicateM n BS.getLine
 
 getNeibor :: Int -> Int -> Int -> Int -> [[Int]]
 getNeibor w' h' w h = do
-  let hs
-        | h' == 1 = [h', h' + 1]
-        | h' == h = [h' - 1, h']
-        | otherwise = [h' - 1, h', h' + 1]
-  let ws
-        | w' == 1 = [w', w' + 1]
-        | w' == w = [w' - 1, w']
-        | otherwise = [w' - 1, w', w' + 1]
+  let hs = [h' - 1, h', h' + 1]
+  let ws = [w' - 1, w', w' + 1]
   filter (\x -> x /= [h', w']) (sequence [hs, ws])
 
 sumBomb :: Array (Int, Int) Char -> [[Int]] -> Int
@@ -52,48 +46,10 @@ calc sss h w i = do
       when (i' /= h * w) $ calc sss h w (i + 1)
     else calc sss h w (i + 1)
 
-calcable :: String -> [Int]
-calcable [] = []
-calcable (s : ss)
-  | s == '#' = 1 : calcable ss
-  | otherwise = 0 : calcable ss
-
-calcSingle :: Array Int Int -> Int -> Int -> String
-calcSingle ss n' n
-  | n' == (n + 1) = []
-  | otherwise = v ++ calcSingle ss (n' + 1) n
-  where
-    v
-      | ss ! n' == 1 = "#"
-      | n' == 1 = show $ ss ! 2
-      | n' == n = show $ ss ! (n - 1)
-      | otherwise = show $ (ss ! (n' - 1)) + (ss ! (n' + 1))
-
-putCharLn :: Char -> IO()
-putCharLn c = putStrLn $ [c]
-
 main = do
   [h, w] <- getIntList
-  if h == 1
-    then do
-      [sss] <- getString
-      if w == 1
-        then if sss == "#" then putStrLn "#" else putStrLn "0"
-        else do
-          let sss' = calcable sss
-          let sss'' = listArray (1, w) sss'
-          putStrLn $ calcSingle sss'' 1 w
-    else do
-      if w == 1
-        then do
-          sss <- getNString h
-          let sss' = calcable (concat (map head sss))
-          let sss'' = listArray (1, h) sss'
-          let result = calcSingle sss'' 1 h
-          mapM_ putCharLn result
-          putStr "\n"
-        else do
-          sss <- getNString h
-          let sss' = concat (map head sss)
-          let sss'' = listArray ((1, 1), (h, w)) sss'
-          calc sss'' h w 0
+  sss <- getNString h
+  let dots = replicate (w + 2) "."
+  let sss' = concat (dots ++ (map (\x -> "." ++ x ++ ".") (map head sss)) ++ dots)
+  let sss'' = listArray ((0, 0), (h + 1, w + 1)) sss'
+  calc sss'' h w 0
