9f444eb3a76a784cffc3ef8f18e4e2308633dc3c
Tsubasa Nagata <nagata.hbdc@gmail.com>
2020-06-14 20:32:55 +0900

    東京海上日動　プログラミングコンテスト2020 C

--- app/tokiomarine2020/C.hs
+++ app/tokiomarine2020/C.hs
@@ -1,10 +1,6 @@
---TLE
 import qualified Data.ByteString.Char8 as BS
 import Data.Maybe
-import qualified Data.Vector.Unboxed.Mutable as VUM
 import qualified Data.Vector.Unboxed as VU
-import Control.Monad.ST
-import Data.Foldable
 
 readInt = fst . fromJust . BS.readInt
 readIntList = map readInt . BS.words
@@ -12,53 +8,22 @@ readIntList = map readInt . BS.words
 getInt = readInt <$> BS.getLine
 getIntList = readIntList <$> BS.getLine
 
-setInitial (a : as) table i = do
-  VUM.write table i a
-  if as /= [] then setInitial as table (i + 1) else return ()
+calc :: VU.Vector Int -> VU.Vector Int
+calc as = do
+  let n = VU.length as
+  let base = VU.generate (n + 1) (\x -> 0)
+  let ds = VU.imap (\i a -> (maximum [0, i - a], 1)) as VU.++ VU.imap (\i a -> (minimum [n, i + a + 1], -1)) as
+  VU.postscanl' (+) 0 (VU.init (VU.accumulate (+) base ds))
 
-writeTT tt (d : ds) = do
-  point <- VUM.unsafeRead tt d
-  VUM.unsafeWrite tt d (point + 1)
-  if ds /= [] then writeTT tt ds else return ()
-
-tableCopy table tt i n = do
-  point <- VUM.unsafeRead tt i
-  VUM.unsafeWrite table i point
-  if i /= n -1 then tableCopy table tt (i + 1) n else return ()
-
-ttClear tt i n = do
-  VUM.write tt i 0
-  if i /= n -1 then ttClear tt (i + 1) n else return ()
-
-cc table tt i n = do
-  point <- VUM.unsafeRead table i
-  let ds = [maximum [i - point, 0] .. minimum [i + point, n -1]]
-  writeTT tt ds
-  if i /= n -1
-    then cc table tt (i + 1) n
-    else do
-      VUM.copy table tt
-      VUM.set tt 0
-
-calc table tt k n = do
-  cc table tt 0 n
-  if k /= 1 then calc table tt (k -1) n else return ()
-
-printVUM table i n = do
-  point <- VUM.unsafeRead table i
-  putStr $ show point
-  if i /= n -1
-    then do
-      putStr " "
-      printVUM table (i + 1) n
-    else putStr " "
+judge :: VU.Vector Int -> Int -> Int -> VU.Vector Int
+judge as n 0 = as
+judge as n k
+  | VU.all (== n) as = as
+  | otherwise = judge (calc as) n (k -1)
 
 main = do
   [n, k] <- getIntList
   as <- getIntList
   let as' = VU.fromList as
-  table <- VUM.replicate n 0 :: IO (VUM.IOVector Int)
-  tt <- VUM.replicate n 0 :: IO (VUM.IOVector Int)
-  for_ [1 .. n] $ \i -> VUM.write table (i -1) (as' VU.! (i - 1))
-  calc table tt k n
-  printVUM table 0 n
+  let as'' = judge as' n k
+  mapM_ putStr (map (\x -> show x ++ " ") (VU.toList as''))
