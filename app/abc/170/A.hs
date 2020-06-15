ecee3cab9e1fb07f6339c1328332e205e4c95f45
Tsubasa Nagata <nagata.hbdc@gmail.com>
2020-06-15 23:19:31 +0900

    ABC 170 A elemIndex利用

--- app/abc/170/A.hs
+++ app/abc/170/A.hs
@@ -1,15 +1,12 @@
 import qualified Data.ByteString.Char8 as BS
 import Data.Maybe
+import Data.List
 
 readInt = fst . fromJust . BS.readInt
 readIntList = map readInt . BS.words
 
 getIntList = readIntList <$> BS.getLine
 
-search :: [Int] -> Int -> Int -> Int
-search (x:xs) n i | x == n = i
-                  | otherwise = search xs n (i+1)
-
 main = do
   xs <- getIntList
-  print $ search xs 0 1
+  print $ fromJust (0 `elemIndex` xs) + 1
